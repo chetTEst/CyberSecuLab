0889eba30c237d1ce7ba2119582c9fffcddfbca2
chetTest <alexu-che@yandex.ru>
2025-01-18 15:43:23 +0300

    Fixed mapping of unique values

--- ksp.st/flask_app/app/models.py
+++ ksp.st/flask_app/app/models.py
@@ -6,6 +6,6 @@ db = SQLAlchemy()
 
 class ShortLink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    short_id = db.Column(db.String(6), unique=True, nullable=False)
+    short_id = db.Column(db.String(6, collation='utf8_bin'), unique=True, nullable=False)
     short_id_int = db.Column(db.Integer, unique=False, nullable=False)
     original_url = db.Column(db.String(2048), nullable=False)
